15fd81d72622698bd80c1e7a018d683c58602c4c
zhangfu <18944639395@163.com>
2017-06-16 18:05:15 +0800

    魔术方法

--- imooc/IMooc/Object.php
+++ imooc/IMooc/Object.php
@@ -7,10 +7,49 @@ namespace IMooc;
 */
 class Object 
 {
-	
-	function test()
+    protected $array=[];
+	public function __set($key,$value)
 	{
-		echo "test01";
+//        	 var_dump(__METHOD__);die;
+		$this->array[$key] = $value;
 	}
+
+	public function __get($key)
+	{
+
+		return $this->array[$key];
+	}
+
+    public function __call($func,$param)
+    {
+
+        var_dump($func,$param);
+
+        return "magic function \n";
+    }
+
+    public static  function __callStatic($func,$param)
+    {
+
+        var_dump($func,$param);
+
+        return "magic static function \n";
+    }
+
+    public function  __toString(){
+
+        return __CLASS__;
+
+
+    }
+
+    public function  __invoke($param){
+
+       var_dump($param);
+
+        return 'invoke';
+
+
+    }
 }
 
--- imooc/index.php
+++ imooc/index.php
@@ -8,7 +8,31 @@ include 'IMooc/Loader.php';
 spl_autoload_register('IMooc\Loader::autoload');
 
 
-IMooc\Object::test();
+// IMooc\Object::test();
 
-App\Controller\Home\Index::test();
+// App\Controller\Home\Index::test();
+
+$obj= new IMooc\Object();
+
+
+//__set __get
+//$obj->title = 'hello';
+//echo $obj->title;
+
+
+//__call
+//$obj->test('hello',123);
+
+
+//__callStatic
+//IMooc\Object::test('hello33',111);
+
+
+//__toString
+
+//echo $obj;
+
+
+//invoke
+echo $obj('kkkk');
 
